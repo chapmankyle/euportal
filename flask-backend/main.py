6bf0964580838f4a1f7587df36ac222fe7101962
Kyle Chapman <20703236@sun.ac.za>
2019-09-01 16:12:11 +0200

    Add product cards

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -7,6 +7,11 @@ def index():
 	"""Renders the index page."""
 	return render_template("index.html", token="Welcome")
 
+@app.route("/products")
+def products():
+	"""Returns the products page."""
+	return render_template("products.html", title="Products")
+
 @app.route("/products/id/<pid>")
 def product(pid):
 	if isinstance(pid, int):

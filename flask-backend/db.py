74619d603be69cc10a541cfea11eba2db2462303
Pierre Henri Nortje <18191436@sun.ac.za>
2019-09-23 21:20:52 +0200

    db connection made, db registration function implemented

--- flask-backend/db.py
+++ flask-backend/db.py
@@ -1,11 +1,32 @@
-# Instance name: modular-decoder-249913:us-central1:euportalsql
-# Instance pswd: rw344euportal
-import MySQLdb
+# Instance name:    modular-decoder-249913:us-central1:euportalsql
+# Instance IP:      35.226.105.22
+# Instance pswd:    rw344euportal
+# gcloud: gcloud sql connect euportalsql --user=root --quiet
+import mysql.connector
+from mysql.connector import Error
 
-conn = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
-cursor = conn.cursor()
+try:
+    connection = mysql.connector.connect(host='35.226.105.22', database='dummy', user='default', password='rw344euportal')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+except Error as e:
+    print("Error while connecting to MySQL", e)
 
-# cursor.execute('SELECT COUNT(MemberID) as count FROM Members WHERE id = 1')
-# row = cursor.fetchone()
-# conn.close()
-# print(row)
+def register_staff(name, password, email, _type, session):
+    cursor = connection.cursor()
+    add_staff = ("INSERT INTO staff (name, password, email, type, session_id) VALUES (%s, %s, %s, %s, %s)")
+    staff_info = (name, password, email, _type, session)
+    cursor.execute(add_staff, staff_info)
+    connection.commit()
+    cursor.close()
+    print("Added new staff user to DB")
+
+def register_customer(firstname, surname, password, email, session):
+    cursor = connection.cursor()
+    add_customer = ("INSERT INTO customers (firstname, surname, password, email, session_id) VALUES (%s, %s, %s, %s, %s)")
+    customer_info = (firstname, surname, password, email, session)
+    cursor.execute(add_customer, customer_info)
+    connection.commit()
+    cursor.close()
+    print("Added new customer user to DB")

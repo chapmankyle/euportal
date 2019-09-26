7fae0c5677417caf021ad0d07f146c0c7ece02f5
Pierre Henri Nortje <18191436@sun.ac.za>
2019-09-26 19:59:13 +0200

    basic DB abstraction implemented, Registeration implemented

--- flask-backend/db.py
+++ flask-backend/db.py
@@ -3,18 +3,22 @@
 # Instance pswd:    rw344euportal
 # gcloud: gcloud sql connect euportalsql --user=root --quiet
 import mysql.connector
-from mysql.connector import Error
+import time
 
 try:
     connection = mysql.connector.connect(host='35.226.105.22', database='dummy', user='default', password='rw344euportal')
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
-except Error as e:
+except Exception as e:
     print("Error while connecting to MySQL", e)
 
-def register_staff(name, password, email, _type, session):
+def generate_session_id(name):
+    return name + str(int(round(time.time() * 1000)))
+
+def register_staff(name, password, email, _type):
     cursor = connection.cursor()
+    session = generate_session_id(name)
     add_staff = ("INSERT INTO staff (name, password, email, type, session_id) VALUES (%s, %s, %s, %s, %s)")
     staff_info = (name, password, email, _type, session)
     cursor.execute(add_staff, staff_info)
@@ -22,8 +26,9 @@ def register_staff(name, password, email, _type, session):
     cursor.close()
     print("Added new staff user to DB")
 
-def register_customer(firstname, surname, password, email, session):
+def register_customer(firstname, surname, password, email):
     cursor = connection.cursor()
+    session = generate_session_id(firstname)
     add_customer = ("INSERT INTO customers (firstname, surname, password, email, session_id) VALUES (%s, %s, %s, %s, %s)")
     customer_info = (firstname, surname, password, email, session)
     cursor.execute(add_customer, customer_info)
--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import json
+import db
 
 app = Flask("__main__")
 
@@ -33,6 +34,15 @@ def other(link):
 	"""Renders the Every other page for react."""
 	return render_template("index.html", token="Welcome")
 
+@app.route("/api/register_customer/<firstname>;<surname>;<email>;<password>")
+def register_customer(firstname, surname, password, email):
+	""" Registers a new customer, returns the session ID """
+	return db.register_customer(firstname, surname, password, email)
+
+@app.route("/api/register_staff/<name>;<password>;<email>;<_type>")
+def register_staff(name, password, email, _type):
+	""" Registers a new staff memeber, returns the session ID """
+	return db.register_staff(name, password, email, _type)
 
 if __name__ == "__main__":
 	"""Run server in debug mode."""

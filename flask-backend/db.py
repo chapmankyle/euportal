8e8569efab852fbae3c680c8169d6812f1e8d116
Liza <liza.okennedy@gmail.com>
2019-09-29 10:44:01 +0200

    Added execute_query function to DB for abstraction

--- flask-backend/db.py
+++ flask-backend/db.py
@@ -6,34 +6,55 @@ import mysql.connector
 import time
 
 try:
-    connection = mysql.connector.connect(host='35.226.105.22', database='dummy', user='default', password='rw344euportal')
+    connection = mysql.connector.connect(
+        host='35.226.105.22', database='dummy', user='default', password='rw344euportal')
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
 except Exception as e:
     print("Error while connecting to MySQL", e)
 
+
 def generate_session_id(name):
     return name + str(int(round(time.time() * 1000)))
 
-def register_staff(name, password, email, _type):
+def execute_query(query, query_info):
     cursor = connection.cursor()
-    session = generate_session_id(name)
-    add_staff = ("INSERT INTO staff (name, password, email, type, session_id) VALUES (%s, %s, %s, %s, %s)")
-    staff_info = (name, password, email, _type, session)
-    cursor.execute(add_staff, staff_info)
+    cursor.execute(query, query_info)
     connection.commit()
     cursor.close()
+
+def register_staff(name, password, email, _type):
+    session = generate_session_id(name)
+    query = (
+        "INSERT INTO staff (name, password, email, type, session_id) VALUES (%s, %s, %s, %s, %s)")
+    query_info = (name, password, email, _type, session)
+    execute_query(query, query_info)
     print("Added new staff user to DB")
     return session
 
+
 def register_customer(firstname, surname, password, email):
-    cursor = connection.cursor()
     session = generate_session_id(firstname)
-    add_customer = ("INSERT INTO customers (firstname, surname, password, email, session_id) VALUES (%s, %s, %s, %s, %s)")
-    customer_info = (firstname, surname, password, email, session)
-    cursor.execute(add_customer, customer_info)
-    connection.commit()
-    cursor.close()
+    query = (
+        "INSERT INTO customers (firstname, surname, password, email, session_id) VALUES (%s, %s, %s, %s, %s)")
+    query_info = (firstname, surname, password, email, session)
+    execute_query(query, query_info)
     print("Added new customer user to DB")
     return session
+
+
+def edit_profile_details(firstname, surname, password, email, session):
+    query = (
+        "UPDATE customers SET firstname=%s, surname=%s, password=%s, email=%s WHERE session_id=%s")
+    query_info = (firstname, surname, password, email, session)
+    execute_query(query, query_info)
+    print("Updated Profile Details")
+
+def get_customer(session):
+    query = ("SELECT * FROM customer WHERE session_id=%s")
+    query_info = (session)
+    execute_query(query, query_info)
+    print("Updated Profile Details")
+
+

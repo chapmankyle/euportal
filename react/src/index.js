e892ae56dc327359bcc70e3822a61a11f9b1bce8
Klensch Lucas <klensch.lucas@gmail.com>
2019-09-30 20:39:07 +0200

    Redux persist

--- react/src/index.js
+++ react/src/index.js
@@ -4,23 +4,44 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./css/index.css";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from 'redux';
 import allReducer from "./reducers/index";
 import Cookies from "universal-cookie"
 
+import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import promise from 'redux-promise';
+
+// defaults to localStorage for web
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
 const cookies = new Cookies();
 
-const store = createStore(
-  allReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const persistedReducer = persistReducer(persistConfig, allReducer);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk, promise))
 );
+let persistor = persistStore(store);
+
+// Must check for session
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
-  
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

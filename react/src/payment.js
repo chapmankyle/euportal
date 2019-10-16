5994cd55a4f7fa04ec00e05adc431114ec3be66c
Liza <liza.okennedy@gmail.com>
2019-10-16 16:49:47 +0200

    added payments page

---
+++ react/src/payment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Tabs, Tab, Container, Row, Jumbotron, Card, Col, Button } from 'react-bootstrap/';
+import Img from 'react-image';
+import profile from './images/profile.png';
+import './css/App.css';
+import { ModalButton } from './templates';
+import EditProfile from './editProfile';
+import cookies from './cookiestore';
+
+class Profile extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    // return (
+    // <>
+    // <>
+    // );
+  }
+}
+
+export default Profile;
